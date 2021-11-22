fb079b772bf40649c55f4f53cca9b6ecd74fa18c
Benjamin Michaelis <benjamin@michaelis.net>
2021-11-22 11:18:48 -0800

    Add Graphing Surface Plane with Vertices

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import Sidebar from "./Sidebar";
 import './App.css';
+import GraphingSurface from './GraphingSurface';
 
 function App() {
   return (
     <div className='App'>
       <Sidebar />
+      <GraphingSurface />
     </div>
   );
 }
