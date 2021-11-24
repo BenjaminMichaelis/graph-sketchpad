98e5ecba0f266332804c8c37b641cbd663791a79
Benjamin Michaelis <benjamin@michaelis.net>
2021-11-23 22:00:43 -0800

    Display Graph Information

---
+++ src/GraphInformation.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './GraphInformation.css'
+
+function GraphInformation(props) {
+    const {numVertices, numEdges, numComponents, graphTypes} = props
+    return (
+      <div className='GraphInformation'>
+          <div>Vertices: <strong>{numVertices}</strong></div>
+          <div>Edges: <strong>{numEdges}</strong></div>
+          <div>Components: <strong>{numComponents}</strong></div>
+      </div>
+    );
+}
+
+export default GraphInformation;
