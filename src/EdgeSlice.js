d0a5e0fccddb0e2a8417c3b1f315c68073e87f84
Benjamin Michaelis <benjamin@michaelis.net>
2021-11-24 20:35:44 -0800

    Button for directed edges implemented
    TODO: Allow color to be changed for each edge.

--- src/EdgeSlice.js
+++ src/EdgeSlice.js
@@ -9,6 +9,7 @@ function EdgeSlice(props)
     const [x1, y1] = [start.position[0] - offsetLeft, start.position[1]]
     const [x2, y2] = [end.position[0] - offsetLeft, end.position[1]]
     const color = edge.color
+    const directedBool = edge.directedBool
 
     let d;
     // loop is determined if the start vertex is equal to the end vertex
@@ -48,47 +49,60 @@ function EdgeSlice(props)
         console.log('angle to degree:',{changeInY,changeInX,radians,degrees})
         return degrees;
     }
-    let polygonTriangleforRight=`${x2-7},${y2-3} ${x2-20},${y2-10} ${x2-20},${y2+10}`
-    // let polygonTriangle=`${x2},${y2+3} ${x2+20},${y2+3} ${x2+12},${y2-10}`
-    let pointss=`${x2},${y2}, ${x2+10},${y2+20}, ${x2},${y2+7}`
 
     // https://github.com/PimpTrizkit/PJs/wiki/12.-Shade,-Blend-and-Convert-a-Web-Color-(pSBC.js)#stackoverflow-archive-begin
-    function shadeHexColor(color, percent)
+    function shadeHexColor(shadeColor, percent)
     {
-        var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
+        var f=parseInt(shadeColor.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
         return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
     }
-    function shadeRGBColor(color, percent)
+    function shadeRGBColor(shadeColor, percent)
     {
-        var f=color.split(","),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=parseInt(f[0].slice(4)),G=parseInt(f[1]),B=parseInt(f[2]);
+        var f=shadeColor.split(","),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=parseInt(f[0].slice(4)),G=parseInt(f[1]),B=parseInt(f[2]);
         return "rgb("+(Math.round((t-R)*p)+R)+","+(Math.round((t-G)*p)+G)+","+(Math.round((t-B)*p)+B)+")";
     }
     // var color1 = shade("rbg(63,131,163)", 0.5);
     // var color2 = shade("#3f83a3", 0.5);
-    function shade(color, percent)
+    function shade(shadeColor, percent)
     {
-        if (color.length > 7 ) return shadeRGBColor(color,percent);
-        else return shadeHexColor(color,percent);
+        if (shadeColor.length > 7 ) return shadeRGBColor(shadeColor,percent);
+        else return shadeHexColor(shadeColor,percent);
     }
 
-    return (
-        <svg>
-            <defs>
-                <marker id="arrowhead" markerWidth="10" markerHeight="7" refX="10" refY="2.625" orient="auto">
-                    <polygon onClick={onClick} fill={shade(color, -.25)} points='0 0, 7.5 2.625, 0 5.25' />
+    var arrowColor = new shade(color, -.25)
+
+    if (directedBool)
+    {
+        return (
+            <svg>
+                <marker id="arrowhead" markerWidth="10" markerHeight="7" refX="9.5" refY="2.625" orient="auto">
+                        <polygon onMouseDown={onClick} fill='#F35347' points='0 0, 7.5 2.625, 0 5.25' />
                 </marker>
-            </defs>
+                <path
+                    marker-end='url(#arrowhead)'
+                    onClick={onClick}
+                    className='Edge-Slice'
+                    fill='none'
+                    stroke={color}
+                    strokeWidth='3'
+                    d={d}
+                    />
+            </svg>
+        )
+    }
+    else
+    {
+        return (
             <path
-                marker-end='url(#arrowhead)'
                 onClick={onClick}
                 className='Edge-Slice'
-                fill='none'
+                fill='transparent'
                 stroke={color}
                 strokeWidth='3'
                 d={d}
-                />
-        </svg>
-    )
+            />
+        )
+    }
 }
 
 export default EdgeSlice;
