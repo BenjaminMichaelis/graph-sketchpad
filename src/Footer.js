9e83bd67143bf195a8d6b6962c321bda2685db31
Benjamin Michaelis <benjamin@michaelis.net>
2021-11-28 22:46:22 -0800

    tiny footer word change

--- src/Footer.js
+++ src/Footer.js
@@ -7,10 +7,10 @@ function Footer()
 {
   return (
     <div class="Footer">
-      <p class="p">Copyright @2021 | Designed by <a href="https://github.com/BenjaminMichaelis">Benjamin Michaelis</a></p>
+      <p class="p">Designed by <a href="https://github.com/BenjaminMichaelis" target="_blank" rel="noopener noreferrer">Benjamin Michaelis</a></p>
       {/* <p class="p">Source Code: <a href="https://github.com/BenjaminMichaelis/GraphSketchpad">https://github.com/BenjaminMichaelis/GraphSketchpad</a></p> */}
             <span class="span">
-                <a href="https://github.com/BenjaminMichaelis/GraphSketchpad">
+                <a href="https://github.com/BenjaminMichaelis/GraphSketchpad" target="_blank" rel="noopener noreferrer">
                     <div class="link">
                         <i class="fab fa-github"></i>
                     </div>
