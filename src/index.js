204a613efa0d23d7fb781b8c3658132eeff4d0de
Benjamin Michaelis <benjamin@michaelis.net>
2024-05-14 12:36:04 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'; 
-
-Sentry.init({
-  dsn: "https://549907e80a8d4359991b8a74aee8247a@o1080390.ingest.sentry.io/6086161",
-  integrations: [new Integrations.BrowserTracing()],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-});
 
 ReactDOM.render(
   <React.StrictMode>
