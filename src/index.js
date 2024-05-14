5a130093a4e7cfbe83ae14c11169dfade7eb9f93
Benjamin Michaelis <benjamin@michaelis.net>
2024-05-14 12:38:15 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Integrations } from "@sentry/tracing";
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
